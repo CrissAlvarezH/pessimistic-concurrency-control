c7cd75b677b2b0eac57a213ed3d7e04bca479e97
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-08-04 21:46:07 +0000

    :construction: WIP add service function to add person to queue

--- persons_queue/models.py
+++ persons_queue/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Queue(models.Model):
--- persons_queue/services.py
+++ persons_queue/services.py
@@ -1,1 +1,30 @@
+from django.db import transaction
+
+from .models import Person, Queue, PersonInQueue
+
+
+def add_person(person_name: str, queue_name: str) -> Person:
+    person = Person.objects.create(name=person_name)
+
+    if queue_name:
+        # create queue if not exists
+        Queue.objects.get_or_create(name=queue_name)
+
+        with transaction.atomic():
+            queue = (
+                Queue.objects
+                .select_for_update()
+                .filter(name=queue_name)
+                .first()
+            )
+
+            last_position = 1
+            last_person = (
+                PersonInQueue.objects
+                .filter(person_id=person.id)
+                .order_by("-created_at")
+                .first()
+            )
+            if last_person:
+                last_position = last_person
 
